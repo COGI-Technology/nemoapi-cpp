25d22d0bd578c1071ee86af3f165af81e0db5a15
Huynh Long Hai <huynhlnghai@gmail.com>
2023-05-26 09:30:22 +0700

    feat: CLI

---
+++ include/cli_config.h
@@ -0,0 +1,4 @@
+// the configured options and settings for CLI
+#define CLI_VERSION_MAJOR 1
+#define CLI_VERSION_MINOR 0
+#define CLI_VERSION_PATCH 0
---
+++ src/cli.cpp
@@ -0,0 +1,37 @@
+#include "nemoapi.h"
+#include "cli_config.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void generate() {
+    EDDSA* dsa = EDDSA::generate();
+    printf("Private Key: %s\n", dsa->prv_as_base64().c_str());
+    printf("Public key: %s\n", dsa->pub_as_base64().c_str());
+    delete dsa;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <command>" << endl;
+        return 1;
+    }
+
+    string command = argv[1];
+
+    if (command == "help") {
+        cout << "Usage" << endl;
+        cout << "- generate" << endl;
+        cout << "- help" << endl;
+    } else if (command == "version") {
+        cout << argv[0] << " Version " << CLI_VERSION_MAJOR << "."
+              << CLI_VERSION_MINOR << "." << CLI_VERSION_PATCH << endl;
+    } else if (command == "generate") {
+        generate();
+    } else {
+        cout << "Unknown command. Type 'help' for available commands." << endl;
+    }
+
+    return 0;
+}
